d0780d94ae6bf743f61ceb5f53a1d5d2f0ef3fa6
Jonas Beck <dev@jonasbeck.dk>
2025-05-28 19:03:50 +0200

    feat(deploy): setup workflow and config to deploy
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jonasbeck.dk",
   vite: {
     plugins: [tailwindcss()],
   },
